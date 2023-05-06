ac89492f9706a093da5eb93c9683426314772b00
bartwroblewski <barti.wroblewski@gmail.com>
2023-05-06 12:14:51 +0200

    'blueprint

--- backend_1_repo/backend1.py
+++ backend_1_repo/backend1.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, Blueprint, redirect
 from mongoengine import Document, fields, connect
 import requests
 
 # TODO:
 # - add some static files like images, JS, css
 # - play with ports in Flask and Fastapi too see the relationship with dockerfile/dockercompose
+# - add Vite/Create React app
+# - add linting? (i.e. fastapi module is not recognized)
 # - how to debug docker image in VS code?
+# - prod vs dev dockerfiles? (i.e. gunicorn vs flask server)
 
 
 app = Flask(__name__)
 connect('dockering', host='mongodb://mongo', username="root", password="example", authentication_source='admin')
+
 
 class Person(Document):
     name = fields.StringField()
@@ -18,16 +22,8 @@ class Person(Document):
 person = Person(name='Barti', age=100)
 person.save()
 
-# startup_person = Person(name='Startup person name', age=99)
-
-# so database needs to run before the app
-# startup_person.save()
-
-# should be other repo and app
 class WorldService:
     def get(self):
-        # raise
-        # return 'world'
         response = requests.get('http://backend2:8000/api/world')
         return response.json()
     
@@ -38,6 +34,15 @@ def hello_api():
     world = world_service.get()
     person = Person.objects.first()
     return f'hellooo {world}, person: {person.name}'
+
+frontend_blueprint = Blueprint('frontend', __name__, root_path="frontend", template_folder="templates")
+
+@frontend_blueprint.route('/')
+def index():
+    # return redirect('http://localhost:5173')
+    return render_template('frontend/index.html')
+
+app.register_blueprint(frontend_blueprint)
 
 if __name__ == '__main__':
     #5000
---
+++ backend_1_repo/frontend/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import 'vite/modulepreload-polyfill'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      This is react app
+    </>
+  )
+}
+
+export default App
