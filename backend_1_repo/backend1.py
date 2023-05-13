14237a1e44acb49622eb086c2e3544bcde0ddf4c
bartwroblewski <barti.wroblewski@gmail.com>
2023-05-13 09:59:29 +0200

    'celery

--- backend_1_repo/backend1.py
+++ backend_1_repo/backend1.py
@@ -1,4 +1,9 @@
+import string
+import time
+
 from flask import Flask, Blueprint
+from celery import Celery, Task, shared_task
+from celery.result import AsyncResult
 from mongoengine import Document, fields, connect
 import requests
 
@@ -12,8 +17,36 @@ import requests
 # - is database persistent among builds? - it is , but why does docker compose up result in two instances of Barti person created?
 # - czemu tworzy folder node_modules/.vite tez na lokalu, gdy docker compose up?
 
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
 
 app = Flask(__name__)
+app.config.from_mapping(
+    CELERY=dict(
+        broker_url="redis://redis",
+        result_backend="redis://redis",
+        task_ignore_result=True,
+    ),
+)
+celery_app = celery_init_app(app)
+
+@shared_task(ignore_result=False)
+def long_blocking_process():
+    alphabet = string.ascii_uppercase
+    for letter in alphabet:
+        Person(name=letter, age=0).save()
+        time.sleep(5)
+
+
 connect('dockering', host='mongodb://mongo', username="root", password="example", authentication_source='admin')
 
 
@@ -21,6 +54,8 @@ class Person(Document):
     name = fields.StringField()
     age = fields.IntField()
 
+for p in Person.objects.all():
+    p.delete()
 person = Person(name='Barti', age=100)
 person.save()
 
@@ -40,6 +75,24 @@ def hello_api():
 @app.route('/echo')
 def echo():
     return {'data': 'hi there'}
+
+@app.route('/long_process')
+def long_process():
+    result = long_blocking_process.delay()
+    return {'result_id': result.id}
+
+@app.get("/result/<id>")
+def task_result(id: str) -> dict[str, object]:
+    result = AsyncResult(id)
+    return {
+        "ready": result.ready(),
+        "successful": result.successful(),
+        "value": result.result if result.ready() else None,
+    }
+
+@app.route('/person')
+def person_route():
+    return {'person_names': [p.name for p in Person.objects.all()]}
 
 frontend_blueprint = Blueprint(
     'frontend',
