a12aab71a51725b037910ec6d911c98ae23ea468
bartwroblewski <barti.wroblewski@gmail.com>
2023-05-14 10:31:24 +0200

    'ok'

--- backend_1_repo/backend1.py
+++ backend_1_repo/backend1.py
@@ -82,8 +82,7 @@ def echo():
 
 @app.route('/long_process')
 def long_process():
-    socketio.emit('long_process_done', {'data': 45454})
-    return 'ok'
+    socketio.emit('long_process_ack', {})
     result = long_blocking_process.delay()
     return {'result_id': result.id}
 
