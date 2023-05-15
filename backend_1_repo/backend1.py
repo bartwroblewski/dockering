2d5a9434a0f9fdd58245a3f5db3fc5f6b14cfecf
bartwroblewski <barti.wroblewski@gmail.com>
2023-05-15 14:53:59 +0200

    'ok

--- backend_1_repo/backend1.py
+++ backend_1_repo/backend1.py
@@ -1,9 +1,10 @@
 import string
 import time
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, redirect, url_for
 from flask_socketio import SocketIO, emit
 from celery import Celery, Task, shared_task
+from celery.signals import task_postrun
 from celery.result import AsyncResult
 from mongoengine import Document, fields, connect
 import requests
@@ -44,15 +45,39 @@ socketio = SocketIO(app)
 
 @shared_task(ignore_result=False)
 def long_blocking_process():
+    print('yo')
     # alphabet = string.ascii_uppercase
     # for letter in alphabet:
     #     Person(name=letter, age=0).save()
     #     time.sleep(5)
-    socketio.emit('long_process_done', {'data': 42})
 
+    # Ideally I would like to use "socketio.emit('long_process_done', {'data': 42})" here directly
+    # instead of sending a request from Celery worker to Flask backend that subsequently
+    # emits a websocket message. This would require figuring out how to create a separate SocketIO instance
+    # that is not bound to Flask app, but communicates over message queue (Redis) instead
+    requests.get('http://backend1:5000/long_process_done')
+
+@app.route('/long_process_done')
+def long_process_done():
+    socketio.emit('long_process_done', {'data': 42})
+    return 'ok'
+
+@app.route('/long_process')
+def long_process():
+    socketio.emit('long_process_ack', {})
+    result = long_blocking_process.delay()
+    return {'result_id': result.id}
+
+@app.get("/result/<id>")
+def task_result(id: str) -> dict[str, object]:
+    result = AsyncResult(id)
+    return {
+        "ready": result.ready(),
+        "successful": result.successful(),
+        "value": result.result if result.ready() else None,
+    }
 
 connect('dockering', host='mongodb://mongo', username="root", password="example", authentication_source='admin')
-
 
 class Person(Document):
     name = fields.StringField()
@@ -79,21 +104,6 @@ def hello_api():
 @app.route('/echo')
 def echo():
     return {'data': 'hi there'}
-
-@app.route('/long_process')
-def long_process():
-    socketio.emit('long_process_ack', {})
-    result = long_blocking_process.delay()
-    return {'result_id': result.id}
-
-@app.get("/result/<id>")
-def task_result(id: str) -> dict[str, object]:
-    result = AsyncResult(id)
-    return {
-        "ready": result.ready(),
-        "successful": result.successful(),
-        "value": result.result if result.ready() else None,
-    }
 
 @app.route('/person')
 def person_route():
